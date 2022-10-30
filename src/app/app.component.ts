a53a4e8fb8d43a843c9de1caa1f0569ff508c008
Oleh Ostrikov <oleh.ostrikov@motorolasolutions.com>
2022-10-30 22:18:15 +0200

    [HIM-test] Installed dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,8 @@ export class AppComponent {
   title = 'openWeather';
   isDay = false;
   is5Days = true;
-
+}
   // update_1 continue from test_1
-  // update_2 from test_2 
+  // update_2 from test_2
            // part_2 and piece_2
+
